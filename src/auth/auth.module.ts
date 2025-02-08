10b4e5da7f6675fb3ff58cbbda94e62f3c41c826
RomanticGuy <109668122+codeTravelerLee@users.noreply.github.com>
2025-02-08 08:02:58 +0000

    에러해결 완료. 구현 끝

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,10 +6,16 @@ import { GoogleAuthGuard } from './auth.guard';
 import { UserModule } from 'src/user/user.module';
 import { UserService } from 'src/user/user.service';
 import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, ConfigModule],
+  imports: [
+    UserModule,
+    ConfigModule,
+    PassportModule.register({ defaultStrategy: 'google', session: false }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy, GoogleAuthGuard]
+  providers: [AuthService, GoogleStrategy, GoogleAuthGuard],
 })
 export class AuthModule {}
